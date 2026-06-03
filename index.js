5951e906ee4b0393c99650d2d2906b90e533f09e
Solange Blanc <solangeblanc@yahoo.com.ar>
2026-06-03 09:33:00 -0300

    mejora de respuestas formato JSON y versionado v1

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // Carga de variables de entorno
 process.loadEnvFile();
 
@@ -7,50 +6,69 @@ import express from 'express';
 import morgan from 'morgan';
 import { requiere_session } from './src/middlewares/requiere_session.js';
 
-
-// Importación de rutas 
+// Importación de rutas
 import especialidadesRutas from './src/rutas/especialidadesRutas.js';
 import obrasSocialesRutas from './src/rutas/obrasSocialesRutas.js';
 import authRutas from './src/rutas/authRutas.js';
 import usuariosRutas from './src/rutas/usuariosRutas.js';
 import turnosRutas from './src/rutas/turnosRutas.js';
 
-
-
 const app = express();
 
 // MIDDLEWARE de CORS para permitir conexiones externas
 app.use(cors({ origin: process.env.CORS_ORIGIN || '*' }));
 
-// MIDDLEWARE para parsear JSON (Necesario para que funcionen los métodos POST y PUT)
+// MIDDLEWARE para parsear JSON
 app.use(express.json());
 
-// MIDDLEWARE Morgan "observa"
+// MIDDLEWARE Morgan
 app.use(morgan('dev'));
 
-// Rutas públicas (No requieren sesión)
+// ====================
+// RUTAS SIN VERSIONADO
+// ====================
+
+// Públicas
 app.use('/auth', authRutas);
-// Rutas de usuarios
 app.use('/usuarios', usuariosRutas);
 
-// Rutas protegidas por sesión
-
-// El MIDDLEWARE requiere_session se asegura de que haya un usuario logueado antes de chequear permisos
+// Protegidas
 app.use('/especialidades', requiere_session, especialidadesRutas);
 app.use('/obras_sociales', requiere_session, obrasSocialesRutas);
 app.use('/turnos', requiere_session, turnosRutas);
 
+// ====================
+// RUTAS VERSIONADAS V1
+// ====================
 
-// Ruta raíz (Bienvenida)
+// Públicas
+app.use('/api/v1/auth', authRutas);
+app.use('/api/v1/usuarios', usuariosRutas);
+
+// Protegidas
+app.use('/api/v1/especialidades', requiere_session, especialidadesRutas);
+app.use('/api/v1/obras_sociales', requiere_session, obrasSocialesRutas);
+app.use('/api/v1/turnos', requiere_session, turnosRutas);
+
+// Ruta raíz
 app.get('/', (req, res) => {
-    res.json({
-        mensaje: "Bienvenido a la API de la Clínica - Grupo AC. Estamos para ayudarlo.",
+    res.status(200).json({
+        api: "Clínica Grupo AC",
+        version: "v1",
+        mensaje: "Bienvenido a la API de la Clínica",
         estado: "Servidor funcionando correctamente",
-        entidades: ["/especialidades", "/medicos", "/obras_sociales","/medicos_obras_sociales", "/pacientes", "/turnos_reservas", "/usuarios"]
+        entidades: [
+            "Usuarios",
+            "Especialidades",
+            "Obras Sociales",
+            "Médicos",
+            "Turnos"
+        ]
     });
 });
 
 const PORT = process.env.PUERTO || 3000;
+
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en http://localhost:${PORT}`);
 });
