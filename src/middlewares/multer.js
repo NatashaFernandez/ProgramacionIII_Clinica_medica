cc843f09f449608a28166430ed788e4d396ee679
Ivonne Fernandez <ivi.fernandez722@gmail.com>
2026-06-15 15:08:57 -0300

    modificacion de multer para guardado de nombres de fotos

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,5 +1,15 @@
 import multer from 'multer';
+import path from 'path';
 
-export const upload = multer({
-    dest: 'uploads'
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); 
+    },
+    filename: (req, file, cb) => {
+        const timestamp = Date.now();
+        const extension = path.extname(file.originalname); 
+        cb(null, `${timestamp}${extension}`);
+    }
 });
+
+export const upload = multer({ storage: storage });
